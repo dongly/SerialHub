b162f141ec659eb9b4a3d1a907a7fd713daec9f5
dongly <chinky@163.com>
2026-04-02 22:04:19 +0800

    feat(tray): 增强托盘菜单功能
    - 添加串口选择子菜单（刷新列表 + 动态串口列表）
    - 添加串口参数子菜单（波特率/数据位/停止位/校验位）
    - 添加网络状态显示（IP + 端口）
    - 添加显示日志功能
    - 实现后台运行模式（自动隐藏控制台）
    - 修复 QuitChan() 和 getIcon() 实现
    - 创建 console_windows.go 控制台控制

---
+++ pkg/tray/console_windows.go
@@ -0,0 +1,45 @@
+package tray
+
+import (
+	"syscall"
+)
+
+var (
+	kernel32             = syscall.NewLazyDLL("kernel32.dll")
+	procGetConsoleWindow = kernel32.NewProc("GetConsoleWindow")
+
+	user32         = syscall.NewLazyDLL("user32.dll")
+	procShowWindow = user32.NewProc("ShowWindow")
+)
+
+const (
+	SW_HIDE = 0
+	SW_SHOW = 5
+)
+
+func GetConsoleWindow() syscall.Handle {
+	ret, _, _ := procGetConsoleWindow.Call()
+	return syscall.Handle(ret)
+}
+
+func ShowWindow(hwnd syscall.Handle, cmdShow int) bool {
+	ret, _, _ := procShowWindow.Call(
+		uintptr(hwnd),
+		uintptr(cmdShow),
+	)
+	return ret != 0
+}
+
+func HideConsole() {
+	hwnd := GetConsoleWindow()
+	if hwnd != 0 {
+		ShowWindow(hwnd, SW_HIDE)
+	}
+}
+
+func ShowConsole() {
+	hwnd := GetConsoleWindow()
+	if hwnd != 0 {
+		ShowWindow(hwnd, SW_SHOW)
+	}
+}
--- pkg/tray/tray.go
+++ pkg/tray/tray.go
@@ -1,10 +1,10 @@
-// Package tray 提供系统托盘功能
 package tray
 
 import (
 	"context"
 	"embed"
 	"fmt"
+	"strconv"
 
 	"github.com/getlantern/systray"
 	"github.com/sirupsen/logrus"
@@ -13,7 +13,6 @@ import (
 	"github.com/yourname/serialhub/pkg/serial"
 )
 
-// TrayState 托盘状态类型
 type TrayState string
 
 const (
@@ -22,32 +21,57 @@ const (
 	TrayError     TrayState = "error"
 )
 
-// TrayManager 系统托盘管理器
 type TrayManager struct {
-	serial     *serial.SerialManager
-	config     *config.Config
-	telnetPort int
-	mcpPort    int
-	version    string
-	state      TrayState
+	serial         *serial.SerialManager
+	config         *config.Config
+	telnetPort     int
+	mcpPort        int
+	version        string
+	state          TrayState
+	quitChan       chan struct{}
+	mSerial        *systray.MenuItem
+	mSelectPort    *systray.MenuItem
+	mRefresh       *systray.MenuItem
+	mPortItems     map[string]*systray.MenuItem
+	mSerialConfig  *systray.MenuItem
+	mBaudRate      *systray.MenuItem
+	mBaudRateItems map[int]*systray.MenuItem
+	mDataBits      *systray.MenuItem
+	mDataBitsItems map[int]*systray.MenuItem
+	mStopBits      *systray.MenuItem
+	mStopBitsItems map[float64]*systray.MenuItem
+	mParity        *systray.MenuItem
+	mParityItems   map[string]*systray.MenuItem
+	mCurrentConfig *systray.MenuItem
+	mNetworkStatus *systray.MenuItem
+	mShowLog       *systray.MenuItem
 }
 
-//go:embed assets/*.png
+var baudRates = []int{9600, 19200, 38400, 57600, 115200, 230400}
+var dataBitsList = []int{5, 6, 7, 8}
+var stopBitsList = []float64{1, 1.5, 2}
+var parityList = []string{"none", "even", "odd"}
+
+//go:embed assets/*.ico
 var iconFS embed.FS
 
-// NewTrayManager 创建托盘管理器
 func NewTrayManager(serialMgr *serial.SerialManager, cfg *config.Config, telnetPort, mcpPort int, version string) *TrayManager {
 	return &TrayManager{
-		serial:     serialMgr,
-		config:     cfg,
-		telnetPort: telnetPort,
-		mcpPort:    mcpPort,
-		version:    version,
-		state:      TrayIdle,
+		serial:         serialMgr,
+		config:         cfg,
+		telnetPort:     telnetPort,
+		mcpPort:        mcpPort,
+		version:        version,
+		state:          TrayIdle,
+		quitChan:       make(chan struct{}),
+		mPortItems:     make(map[string]*systray.MenuItem),
+		mBaudRateItems: make(map[int]*systray.MenuItem),
+		mDataBitsItems: make(map[int]*systray.MenuItem),
+		mStopBitsItems: make(map[float64]*systray.MenuItem),
+		mParityItems:   make(map[string]*systray.MenuItem),
 	}
 }
 
-// Run 启动系统托盘（必须在主 goroutine 调用）
 func (t *TrayManager) Run(ctx context.Context) {
 	systray.Run(func() {
 		t.onReady()
@@ -56,56 +80,330 @@ func (t *TrayManager) Run(ctx context.Context) {
 	})
 }
 
-// onReady 托盘就绪回调
 func (t *TrayManager) onReady() {
 	logrus.Info("[SerialHub] 系统托盘已启动")
 
-	// 设置图标
 	systray.SetIcon(t.getIcon())
 	systray.SetTitle("SerialHub")
 	systray.SetTooltip(fmt.Sprintf("SerialHub v%s", t.version))
 
-	// 创建菜单
-	mSerial := systray.AddMenuItem("📡 串口连接", "串口连接")
-	mConfig := systray.AddMenuItem("⚙️ 串口参数", "串口参数")
-	mPorts := systray.AddMenuItem("🌐 服务端口", "服务端口")
-	systray.AddSeparator()
-	mVersion := systray.AddMenuItem(fmt.Sprintf("📋 版本 %s", t.version), "版本")
-	mVersion.Disable()
-	systray.AddSeparator()
-	mQuit := systray.AddMenuItem("❌ 退出", "退出 SerialHub")
+	t.createMenu()
+	t.setupEventHandlers()
+	t.refreshPortList()
+	t.updateConfigDisplay()
+}
 
-	// 处理菜单点击
-	go func() {
-		for {
-			select {
-			case <-mQuit.ClickedCh:
-				systray.Quit()
-				return
-			case <-mSerial.ClickedCh:
-				t.toggleSerial()
-			case <-mConfig.ClickedCh:
-				t.showConfig()
-			case <-mPorts.ClickedCh:
-				t.showPorts()
-			}
+func (t *TrayManager) createMenu() {
+	t.mSerial = systray.AddMenuItem(t.getSerialMenuTitle(), "串口连接")
+
+	t.mSelectPort = systray.AddMenuItem("选择串口", "选择串口")
+	t.mRefresh = t.mSelectPort.AddSubMenuItem("刷新列表", "刷新串口列表")
+	systray.AddSeparator()
+
+	t.mSerialConfig = systray.AddMenuItem("串口参数", "串口参数")
+
+	t.mBaudRate = t.mSerialConfig.AddSubMenuItem("波特率", "选择波特率")
+	for _, rate := range baudRates {
+		item := t.mBaudRate.AddSubMenuItem(strconv.Itoa(rate), fmt.Sprintf("波特率 %d", rate))
+		t.mBaudRateItems[rate] = item
+	}
+
+	t.mDataBits = t.mSerialConfig.AddSubMenuItem("数据位", "选择数据位")
+	for _, bits := range dataBitsList {
+		item := t.mDataBits.AddSubMenuItem(strconv.Itoa(bits), fmt.Sprintf("数据位 %d", bits))
+		t.mDataBitsItems[bits] = item
+	}
+
+	t.mStopBits = t.mSerialConfig.AddSubMenuItem("停止位", "选择停止位")
+	for _, bits := range stopBitsList {
+		label := fmt.Sprintf("%.0f", bits)
+		if bits == 1.5 {
+			label = "1.5"
 		}
+		item := t.mStopBits.AddSubMenuItem(label, fmt.Sprintf("停止位 %s", label))
+		t.mStopBitsItems[bits] = item
+	}
+
+	t.mParity = t.mSerialConfig.AddSubMenuItem("校验位", "选择校验位")
+	for _, p := range parityList {
+		item := t.mParity.AddSubMenuItem(p, fmt.Sprintf("校验位 %s", p))
+		t.mParityItems[p] = item
+	}
+
+	t.mCurrentConfig = t.mSerialConfig.AddSubMenuItem(t.getConfigSummary(), "当前配置")
+	t.mCurrentConfig.Disable()
+
+	systray.AddSeparator()
+
+	t.mNetworkStatus = systray.AddMenuItem(t.getNetworkStatus(), "网络状态")
+	t.mNetworkStatus.Disable()
+
+	systray.AddSeparator()
+
+	t.mShowLog = systray.AddMenuItem("显示日志", "显示日志窗口")
+
+	systray.AddSeparator()
+
+	mVersion := systray.AddMenuItem(fmt.Sprintf("版本 %s", t.version), "版本")
+	mVersion.Disable()
+
+	systray.AddSeparator()
+
+	mQuit := systray.AddMenuItem("退出", "退出 SerialHub")
+
+	go func() {
+		<-mQuit.ClickedCh
+		systray.Quit()
 	}()
 }
 
-// onExit 托盘退出回调
-func (t *TrayManager) onExit() {
-	logrus.Info("[SerialHub] 系统托盘已退出")
+func (t *TrayManager) setupEventHandlers() {
+	go t.handleRefresh()
+	go t.handleSerialToggle()
+	go t.handleShowLog()
+	go t.handleBaudRateSelection()
+	go t.handleDataBitsSelection()
+	go t.handleStopBitsSelection()
+	go t.handleParitySelection()
+	go t.handlePortSelection()
 }
 
-// UpdateState 更新托盘状态
+func (t *TrayManager) handleRefresh() {
+	for range t.mRefresh.ClickedCh {
+		t.refreshPortList()
+	}
+}
+
+func (t *TrayManager) handleSerialToggle() {
+	for range t.mSerial.ClickedCh {
+		t.toggleSerial()
+	}
+}
+
+func (t *TrayManager) handleShowLog() {
+	for range t.mShowLog.ClickedCh {
+		ShowConsole()
+	}
+}
+
+func (t *TrayManager) handleBaudRateSelection() {
+	for rate, item := range t.mBaudRateItems {
+		go func(r int, i *systray.MenuItem) {
+			for range i.ClickedCh {
+				t.setBaudRate(r)
+			}
+		}(rate, item)
+	}
+}
+
+func (t *TrayManager) handleDataBitsSelection() {
+	for bits, item := range t.mDataBitsItems {
+		go func(b int, i *systray.MenuItem) {
+			for range i.ClickedCh {
+				t.setDataBits(b)
+			}
+		}(bits, item)
+	}
+}
+
+func (t *TrayManager) handleStopBitsSelection() {
+	for bits, item := range t.mStopBitsItems {
+		go func(b float64, i *systray.MenuItem) {
+			for range i.ClickedCh {
+				t.setStopBits(b)
+			}
+		}(bits, item)
+	}
+}
+
+func (t *TrayManager) handleParitySelection() {
+	for parity, item := range t.mParityItems {
+		go func(p string, i *systray.MenuItem) {
+			for range i.ClickedCh {
+				t.setParity(p)
+			}
+		}(parity, item)
+	}
+}
+
+func (t *TrayManager) handlePortSelection() {
+	for port, item := range t.mPortItems {
+		go func(p string, i *systray.MenuItem) {
+			for range i.ClickedCh {
+				t.setPort(p)
+			}
+		}(port, item)
+	}
+}
+
+func (t *TrayManager) refreshPortList() {
+	ports, err := t.serial.ListPorts()
+	if err != nil {
+		logrus.Warnf("[SerialHub] 获取串口列表失败: %v", err)
+		return
+	}
+
+	for _, item := range t.mPortItems {
+		item.Hide()
+	}
+	t.mPortItems = make(map[string]*systray.MenuItem)
+
+	for _, port := range ports {
+		item := t.mSelectPort.AddSubMenuItem(port, fmt.Sprintf("选择串口 %s", port))
+		t.mPortItems[port] = item
+		if port == t.config.Serial.Port {
+			item.Check()
+		}
+		go func(p string, i *systray.MenuItem) {
+			for range i.ClickedCh {
+				t.setPort(p)
+			}
+		}(port, item)
+	}
+
+	logrus.Infof("[SerialHub] 刷新串口列表: %d 个串口", len(ports))
+}
+
+func (t *TrayManager) setPort(port string) {
+	if t.serial.IsConnected() {
+		logrus.Warn("[SerialHub] 请先断开串口连接再切换串口")
+		return
+	}
+
+	for p, item := range t.mPortItems {
+		if p == port {
+			item.Check()
+		} else {
+			item.Uncheck()
+		}
+	}
+
+	t.config.Serial.Port = port
+	logrus.Infof("[SerialHub] 切换串口: %s", port)
+}
+
+func (t *TrayManager) setBaudRate(rate int) {
+	if t.serial.IsConnected() {
+		logrus.Warn("[SerialHub] 请先断开串口连接再修改波特率")
+		return
+	}
+
+	for r, item := range t.mBaudRateItems {
+		if r == rate {
+			item.Check()
+		} else {
+			item.Uncheck()
+		}
+	}
+
+	t.config.Serial.BaudRate = rate
+	t.updateConfigDisplay()
+	logrus.Infof("[SerialHub] 设置波特率: %d", rate)
+}
+
+func (t *TrayManager) setDataBits(bits int) {
+	if t.serial.IsConnected() {
+		logrus.Warn("[SerialHub] 请先断开串口连接再修改数据位")
+		return
+	}
+
+	for b, item := range t.mDataBitsItems {
+		if b == bits {
+			item.Check()
+		} else {
+			item.Uncheck()
+		}
+	}
+
+	t.config.Serial.DataBits = bits
+	t.updateConfigDisplay()
+	logrus.Infof("[SerialHub] 设置数据位: %d", bits)
+}
+
+func (t *TrayManager) setStopBits(bits float64) {
+	if t.serial.IsConnected() {
+		logrus.Warn("[SerialHub] 请先断开串口连接再修改停止位")
+		return
+	}
+
+	for b, item := range t.mStopBitsItems {
+		if b == bits {
+			item.Check()
+		} else {
+			item.Uncheck()
+		}
+	}
+
+	t.config.Serial.StopBits = int(bits)
+	t.updateConfigDisplay()
+	logrus.Infof("[SerialHub] 设置停止位: %.1f", bits)
+}
+
+func (t *TrayManager) setParity(parity string) {
+	if t.serial.IsConnected() {
+		logrus.Warn("[SerialHub] 请先断开串口连接再修改校验位")
+		return
+	}
+
+	for p, item := range t.mParityItems {
+		if p == parity {
+			item.Check()
+		} else {
+			item.Uncheck()
+		}
+	}
+
+	t.config.Serial.Parity = parity
+	t.updateConfigDisplay()
+	logrus.Infof("[SerialHub] 设置校验位: %s", parity)
+}
+
+func (t *TrayManager) updateConfigDisplay() {
+	t.mCurrentConfig.SetTitle(t.getConfigSummary())
+}
+
+func (t *TrayManager) getConfigSummary() string {
+	parity := t.config.Serial.Parity
+	if parity == "none" {
+		parity = "N"
+	} else if parity == "even" {
+		parity = "E"
+	} else if parity == "odd" {
+		parity = "O"
+	}
+	return fmt.Sprintf("当前: %d %d%s%d",
+		t.config.Serial.BaudRate,
+		t.config.Serial.DataBits,
+		parity,
+		t.config.Serial.StopBits)
+}
+
+func (t *TrayManager) getNetworkStatus() string {
+	return fmt.Sprintf("127.0.0.1 | Telnet:%d | MCP:%d", t.telnetPort, t.mcpPort)
+}
+
+func (t *TrayManager) getSerialMenuTitle() string {
+	if t.serial.IsConnected() {
+		return fmt.Sprintf("断开 %s", t.serial.CurrentPort())
+	}
+	return fmt.Sprintf("连接 %s", t.config.Serial.Port)
+}
+
+func (t *TrayManager) onExit() {
+	logrus.Info("[SerialHub] 系统托盘已退出")
+	close(t.quitChan)
+}
+
+func (t *TrayManager) QuitChan() <-chan struct{} {
+	return t.quitChan
+}
+
 func (t *TrayManager) UpdateState(state TrayState) {
 	t.state = state
 	systray.SetIcon(t.getIcon())
 	logrus.Infof("[SerialHub] 托盘状态更新: %s", state)
 }
 
-// UpdateSerialStatus 根据串口状态更新托盘
 func (t *TrayManager) UpdateSerialStatus() {
 	if t.serial.IsConnected() {
 		t.UpdateState(TrayConnected)
@@ -114,16 +412,28 @@ func (t *TrayManager) UpdateSerialStatus() {
 		t.UpdateState(TrayIdle)
 		systray.SetTooltip("SerialHub - 未连接")
 	}
+	t.mSerial.SetTitle(t.getSerialMenuTitle())
 }
 
-// getIcon 根据状态返回图标
 func (t *TrayManager) getIcon() []byte {
-	// 简化实现，返回空图标
-	// 实际应该从 embed.FS 加载
-	return []byte{}
+	var iconPath string
+	switch t.state {
+	case TrayConnected:
+		iconPath = "assets/tray-connected.ico"
+	case TrayError:
+		iconPath = "assets/tray-error.ico"
+	default:
+		iconPath = "assets/tray-idle.ico"
+	}
+
+	data, err := iconFS.ReadFile(iconPath)
+	if err != nil {
+		logrus.Warnf("[SerialHub] 加载图标失败: %v", err)
+		return []byte{}
+	}
+	return data
 }
 
-// toggleSerial 切换串口连接
 func (t *TrayManager) toggleSerial() {
 	if t.serial.IsConnected() {
 		if err := t.serial.Disconnect(); err != nil {
@@ -139,14 +449,4 @@ func (t *TrayManager) toggleSerial() {
 		}
 	}
 	t.UpdateSerialStatus()
-}
-
-// showConfig 显示配置菜单
-func (t *TrayManager) showConfig() {
-	logrus.Debug("[SerialHub] 显示串口参数菜单")
-}
-
-// showPorts 显示端口菜单
-func (t *TrayManager) showPorts() {
-	logrus.Debugf("[SerialHub] Telnet端口: %d, MCP端口: %d", t.telnetPort, t.mcpPort)
 }
